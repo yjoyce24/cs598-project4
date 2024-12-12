604f6e5216f3e33c9a85d7f1425e439cb83da993
Joyce Yan <joyceyan@live.unc.edu>
2024-12-12 11:06:06 -0500

    add reset label

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def create_star_rating(Title, MovieID):
         # st.write(f"**{value}** stars!")
 
     stars = st_star_rating(label, amount_of_stars, default_value, size, emoticons, read_only, dark_theme,
-                           resetButton=True, resetLabel="",
+                           resetButton=True, resetLabel="reset rating",
                                customCSS=customcss, on_click=function_to_run_on_click)
     st.write(stars)
 
