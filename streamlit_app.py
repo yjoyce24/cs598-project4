e3a097d967d67a08b73e376ef6a3d38ca0032dd0
Joyce Yan <joyceyan@live.unc.edu>
2024-12-12 14:12:28 -0500

    test bold again

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def create_star_rating(Title, MovieID):
                                customCSS=customcss, on_click=function_to_run_on_click)
 
 
-rate_movies_exp = st.expander("**Add Movie Ratings**", expanded=True)
+rate_movies_exp = st.expander(rf"""**Add Movie Ratings**""", expanded=True)
 
 with rate_movies_exp.container(height = 400):
     i = 0
