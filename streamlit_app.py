469e0003e5f724c74066e3bbb2af12c3b7233498
Joyce Yan <joyceyan@live.unc.edu>
2024-12-12 13:46:55 -0500

    try out expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,10 @@ def create_star_rating(Title, MovieID):
                                customCSS=customcss, on_click=function_to_run_on_click)
 
 
-with st.container(height = 400):
+
+exp= st.expander("Step 1: Rate Movies", expanded=False)
+
+with exp.container(height = 400):
     i = 0
 
     for col in st.columns(5) + st.columns(5) + st.columns(5) + st.columns(5) + st.columns(5) + \
